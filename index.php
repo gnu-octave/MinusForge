b538d26e98e879b910e98d422e0c7b3f086bb794
Kai T. Ohlhus <k.ohlhus@gmail.com>
2022-06-27 01:57:35 +0900

    index.php: first implementation

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+$CACHE_FILE = "cache.txt";
+$OCTAVE_PACKAGES_URL = "https://gnu-octave.github.io/packages/packages/forge-download.html";
+
+// Delete outdated cache file.
+if (is_file($CACHE_FILE)) {
+  if (time() - filemtime($CACHE_FILE) >= 60 * 60 * 24) {  // 1 day
+    unlink($CACHE_FILE);
+    print("Clear cache");
+  }
+}
+
+// Recreate cache if necessary.
+if (! is_file($CACHE_FILE)) {
+  file_put_contents($CACHE_FILE, fopen($OCTAVE_PACKAGES_URL, 'r'));
+  print("Create cache");
+}
+
+// Find entry in cache file.
+$tarball = $_GET['file'];
+$str = file_get_contents($CACHE_FILE);
+preg_match('/^' . $tarball . ',(.*)$/m', $str, $redirect_url);
+
+if (! $redirect_url) {
+  header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
+} else {
+  // Temporary redirect
+  header("Location: " . $redirect_url[1], true, 307);
+}
+exit();
+?>
