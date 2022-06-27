4655367a833537a6e6356e5fbeb62bf289a54806
Kai T. Ohlhus <k.ohlhus@gmail.com>
2022-06-28 00:30:18 +0900

    Improve rules

--- download.php
+++ download.php
@@ -1,6 +1,13 @@
 <?php
 $CACHE_FILE = "cache.txt";
-$OCTAVE_PACKAGES_URL = "https://gnu-octave.github.io/packages/packages/forge-download.html";
+$OCTAVE_PACKAGES_URL = "https://gnu-octave.github.io/packages";
+$PACKAGES_DOWNLOAD_URL = "$OCTAVE_PACKAGES_URL/packages/forge-download.html";
+
+// Just redirect if no tarball is requested.
+if (! isset($_GET["file"])) {
+  header("404 Not Found", true, 404);
+	exit();
+}
 
 // Delete outdated cache file.
 if (is_file($CACHE_FILE)) {
@@ -11,7 +18,7 @@ if (is_file($CACHE_FILE)) {
 
 // Recreate cache if necessary.
 if (! is_file($CACHE_FILE)) {
-  file_put_contents($CACHE_FILE, fopen($OCTAVE_PACKAGES_URL, 'r'));
+  file_put_contents($CACHE_FILE, fopen($PACKAGES_DOWNLOAD_URL, 'r'));
 }
 
 // Find entry in cache file.
@@ -20,7 +27,7 @@ $str = file_get_contents($CACHE_FILE);
 preg_match('/^' . $tarball . ',(.*)$/m', $str, $redirect_url);
 
 if (! $redirect_url) {
-  header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
+  header("404 Not Found", true, 404);
 } else {
   // Temporary redirect
   header("Location: " . $redirect_url[1], true, 307);
